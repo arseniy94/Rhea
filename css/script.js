3674187dc393cb110499e94b3410d53173f6eb25
arseniy94 <61457171+arseniy94@users.noreply.github.com>
2020-07-27 00:06:26 +0700

    исправил навигацию

--- css/script.js
+++ css/script.js
@@ -1,8 +1,1 @@
-menu.onclick = function myFnction() {
-  var x = document.getElementById("my_header");
-  if (x.className === "header_nav") {
-    x.className += " responsive";
-  } else {
-    x.className = "header_nav";
-  }
-};
+
